c5c3fe24971e97f74a4b8d375779dfceab48e926
Jean-Léo <160662559+jeanleos@users.noreply.github.com>
2025-03-13 11:49:00 +0100

    Test window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use bevy::{prelude::*, window::WindowResized};
+use bevy::prelude::*;
+#[cfg(not(target_arch = "wasm32"))]
+use bevy::sprite::{Wireframe2dConfig, Wireframe2dPlugin};
 use libnoise::prelude::*;
 
 static STEP: f64 = 0.1;
+const X_EXTENT: f32 = 900.;
+
 
 fn print_noise() {
     let perlin = Source::simplex(42);
@@ -17,15 +21,51 @@ fn print_noise() {
     }
 }
 
-fn set_window(keys: Res<ButtonInput<KeyCode>>,
-    mut window: Single<&mut Window>,
-    resolution: Res<ResolutionSettings>,) {
-    window.resolution.set(200.0, 350.0);
+fn setup(mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+    let shapes = [
+        meshes.add(Rectangle::new(50.0, 100.0))
+    ];
+
+    let num_shapes = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        // Distribute colors evenly across the rainbow.
+        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+
+        commands.spawn((
+            Mesh2d(shape),
+            MeshMaterial2d(materials.add(color)),
+            Transform::from_xyz(
+                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+                0.0,
+                0.0,
+            ),
+        ));
+    }
 }
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (set_window, print_noise))
-        .run();
+    let mut app = App::new();
+        /*.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: [480., 360.].into(),
+                title: "Rust".to_string(),
+                ..default()
+            }),
+            ..default()
+        }), 
+            #[cfg(not(target_arch = "wasm32"))]
+            Wireframe2dPlugin)*/
+    app.add_plugins(DefaultPlugins);
+    app.add_plugins(
+        #[cfg(not(target_arch = "wasm32"))]
+        Wireframe2dPlugin
+    );
+    app.add_systems(Startup, (print_noise, setup));
+    app.run();
 }
